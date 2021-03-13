a8154e174cb913192b6fdfd5d094f24b9928a4a3
vippriya <priya@futurenoob.io>
2021-03-13 13:53:30 +0100

    updated notiifcations

---
+++ renderer/helper/helper.js
@@ -0,0 +1,111 @@
+
+const { ipcMain , ipcRenderer } = require('electron')
+const path = require('path')
+const axios = require('axios')
+
+const INIT_MAP = {
+    product: {
+        formId: 'productInitForm',
+        eventName: 'init-all-products',
+        resourceName: "product",
+        fileName: "./products/product.html",
+        addResourceWindowEventName : "add-product-window",
+        createResourceViewBtn: 'createProductBtn'
+    },
+    notification: {
+        formId: 'notificationInitForm',
+        eventName: 'init-all-notifications',
+        resourceName: "notification",
+        fileName: "./notification/notification.html",
+        addResourceWindowEventName : "add-notification-window",
+        createResourceViewBtn: "createNotificationBtn"
+    }
+}
+function registerAddResourceWindowHandler(key, mainWindow){
+     // create add notificaiton window
+   const {fileName , resourceName, addResourceWindowEventName }= INIT_MAP[key];
+   let addTodoWin ;
+  ipcMain.on(addResourceWindowEventName, () => {
+    // if addTodoWin does not already exist
+    if (!addTodoWin) {
+      // create a new add todo window
+      addTodoWin = new Window({
+        file: path.join('renderer', fileName),
+        width: 900,
+        height: 900,
+        // close with the main window
+        parent: mainWindow
+      })
+
+      // cleanup
+      addTodoWin.on('closed', () => {
+        addTodoWin = null
+      })
+    }
+  })
+}
+
+
+function registerViewResourceWindow(key){
+    // create add notification window button
+     const {createResourceViewBtn, addResourceWindowEventName }= INIT_MAP[key];
+    document.getElementById(createResourceViewBtn).addEventListener('click', () => {
+        ipcRenderer.send(addResourceWindowEventName )
+    })
+}
+
+const config = {
+	timeout: 10000,
+	headers: {
+		'Access-Control-Allow-Origin': '*'
+	}
+};
+const axiosRef =  axios.create(config);
+async function initData(resourceName) {
+    let response;
+    console.log("::::::::iniTData::::::");
+    try {
+    const params = {
+        method: "post",
+        url: "http://localhost:9000"+ resourceName+"/fn_fillAll",
+        retry: 1,
+        retryDelay: 1000
+    }
+        response = await axiosRef.request(params);
+        console.error("::::::::done::::::");
+    } catch (e) {
+        console.error("::::::::error::::::", error);
+    }
+    return response ?   response.data: response;
+}
+
+function registerEventHandler(key){
+      // create add notificaiton window
+    const {eventName, resourceName }= INIT_MAP[key];
+    ipcMain.on(eventName, (event, todo) => {
+   console.log("on....................")
+    console.log("#####################init all")
+   // const updatedTodos = todosData.addTodo(todo).todos;
+    initData(resourceName)
+    //mainWindow.send('todos', updatedTodos)
+  })
+
+}
+function registerInitEvent(key){
+  const {formId, eventName} = INIT_MAP[key];
+    document.getElementById(formId).addEventListener('submit', (evt) => {
+      // prevent default refresh functionality of forms
+      evt.preventDefault()
+      alert("do it----------------- -------------")
+      // send todo to main process
+      ipcRenderer.send(eventName, "input.value")
+  })
+}
+
+const helpers  = {
+    registerInitEvent,
+    registerViewResourceWindow,
+    registerAddResourceWindowHandler,
+    registerEventHandler
+}
+module.exports = helpers;
---
+++ renderer/products/product.js
@@ -0,0 +1,6 @@
+
+const { ipcRenderer } = require('electron')
+const axios = require('axios');
+const helpers = require('../helper/helper');
+
+helpers.registerInitEvent("product")
